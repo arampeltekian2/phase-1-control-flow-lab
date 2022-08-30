284770b8aeefb63d55e33d7e1dde1d1481193b1c
Aram <arampeltekian2@gmail.com>
2022-08-30 13:58:47 -0500

    commit tree 7ff6b9d6da62e55df962da8850f9f5b1e1a4cdb4

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet (distance){
+  if(distance<=400) {
+    return "This one is on me!";
+  } 
+  else if (distance < 2000){
+    return "That will be twenty bucks.";
+  } 
+  else if (2500 >= 2000){
+    return "I will galdly take your thirty bucks."; 
+  } 
+  else if (2501 >= distance){
+    return "No can do.";
+  }
+}
+    
+
+
+function ternaryCheckCity(city){
+return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case switchOnCharmFromTip: 
+        tip === "generous"; 
+        return "Thank you so much."
+   
+      case switchOnCharmFromTip:
+        tip `notAsGenerous`
+        return 'Thank you.';
+    
+      case switchOnCharmFromTip:
+        thanksForEverything = 'Bye.';
+ 
+  }
 }
